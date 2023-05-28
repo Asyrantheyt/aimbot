f92bbe874a39d85b484dae3502b96b38656bd52a
Asyrantheyt <94661066+Asyrantheyt@users.noreply.github.com>
2023-05-28 18:07:11 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,14 +1,19 @@
 local uis = game:GetService("UserInputService")
 local ts = game:GetService("TweenService")
+local lplr = game:GetService("Players").LocalPlayer
 
 _G.aim = false
+_G.autoclickwhileaim = true
 _G.KeyBind = Enum.KeyCode.F
 local yay,sus = pcall(function()
 	function getclosest()
+	print("in getclo function")
 	    local clodis = math.huge
 	    local cloplr
 	    for i,v in pairs(game:GetService("Players"):GetPlayers()) do
-	        if v ~= game:GetService("Players").LocalPlayer and v.TeamColor ~= game:GetService("Players").LocalPlayer.TeamColor and v.Character:FindFirstChild("HumanoidRootPart") and v.Character.Humanoid.Health ~= 0 then
+	    print("out if")
+	        if v ~= game:GetService("Players").LocalPlayer and v.TeamColor ~= game:GetService("Players").LocalPlayer.TeamColor and v.Character and v.Character:FindFirstChild("HumanoidRootPart") and v.Character.Humanoid.Health ~= 0 then
+	        print("in if")
 	            local distance = (game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position - v.Character.HumanoidRootPart.Position).Magnitude
 	            if distance < clodis then
 	                clodis = distance
@@ -25,8 +30,26 @@ local yay,sus = pcall(function()
 	        _G.aim = true
 	        while _G.aim == true do
 	            local camera = game:GetService("Workspace").CurrentCamera
-	            if getclosest().ClassName == "Player" and getclosest().Character:FindFirstChild("Head") and getclosest().TeamColor ~= game:GetService("Players").LocalPlayer.TeamColor then
+	            if _G.autoclickwhileaim == true and getclosest().ClassName == "Player" and getclosest().Character:FindFirstChild("Head") and getclosest().TeamColor ~= game:GetService("Players").LocalPlayer.TeamColor then
 	                ts:Create(camera,TweenInfo.new(0.25), {CFrame = CFrame.new(camera.CFrame.Position, getclosest().Character.Head.Position)}):Play()
+	                if lplr:GetMouse().Target and getclosest().Character.Parent:FindFirstChild(lplr:GetMouse().Target.Parent.Name) then 
+	                	local person = getclosest().Character.Parent:FindFirstChild(lplr:GetMouse().Target.Parent.Name)
+	                	if type(mouse1down) ~= 'function' then
+		                	mouse1press() 
+		                	elseif type(mouse1press) ~= 'function' then 
+		                	mouse1down() 
+		                	else 
+		                	print("missing mouse1down or mouse1press") 
+	                	end
+	                	game:GetService("RunService").RenderStepped:Wait()
+	                	if type(mouse1up) ~= 'function' then 
+		                	mouse1release() 
+		                	elseif type(mouse1release) ~= 'function' then
+		                	mouse1up()
+		                	else
+		                	print("missing mouse1up or mouse1release")
+	                	end
+	                end
 	                uis.InputEnded:Connect(function(input)
 	                    if input.KeyCode == _G.KeyBind then
 	                        _G.aim = false
@@ -45,4 +68,4 @@ local yay,sus = pcall(function()
 	    end
 	end)
 end)
-if sus then warn(sus) and error(sus) end
+if sus then warn(sus) error(sus) end
